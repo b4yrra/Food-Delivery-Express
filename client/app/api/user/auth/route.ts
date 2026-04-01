d9c72e21a53373d89965adb4cabb06542ca726b3
25lp5214 User <25lp5214@Administrators-iMac.local>
2026-04-01 12:47:31 +0800

    QWEQWEQWE

---
+++ client/app/api/user/auth/route.ts
@@ -0,0 +1,24 @@
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export type SignInResponse = {
+  secretToken: string;
+};
+
+export async function POST(request: NextRequest) {
+  const credentials = await request.json();
+  const cookieStore = await cookies();
+
+  const response = await fetch("http://localhost:3000/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  });
+
+  const data = (await response.json()) as SignInResponse;
+  cookieStore.set("token", data.secretToken);
+
+  return NextResponse.json({ success: true });
+}
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,9 +2,12 @@ import { SignIn } from "./_components/SignIn";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex">
       <div>
         <SignIn />
+      </div>
+      <div className="w-[856px] h-[904px]">
+        <img src="./batdel.jpg" alt="BatDelivery" />
       </div>
     </div>
   );
